6532a64ea1f65d9ea23cfcaa594d6cd0b4c648c7
madalyn-benson <112671039+madalyn-benson@users.noreply.github.com>
2022-09-21 00:14:55 -0400

    Update case_study_01.R

--- week_01/case_study_01.R
+++ week_01/case_study_01.R
@@ -1,2 +1,11 @@
 data(iris)
+install.packages("dplyr")
+library(dplyr)
+data(iris)
+head(iris)
+iris$Petal.Length
+selected_Column<-dplyr::select(iris, Petal.Length)
+iris[,3]
+petal_length_mean<-mean(iris[,3])
+hist(iris[,3], main="Petal Length Histogram",xlab="Petal Length",col="green")
 
