4064ae35ee772712a2e461780e358286395237d5
madalyn-benson <112671039+madalyn-benson@users.noreply.github.com>
2022-09-22 13:15:23 -0400

    Update case_study_03.R

--- week_03/case_study_03.R
+++ week_03/case_study_03.R
@@ -1,4 +1,4 @@
- library(ggplot2)
+library(ggplot2)
 library(gapminder) 
 library(dplyr)
  
@@ -16,26 +16,20 @@ plot1 <- ggplot(gapminder_without_Kuwait, aes(x=lifeExp, y= gdpPercap))+
 
 plot1
 
-ggsave("plot1.png", width=20, units="in")
+ggsave("plot1.png", width=15, units="in")
 
 gapminder_continent <- gapminder_without_Kuwait %>%
   group_by(continent,year) %>%
   filter(gdpPercap<=50000) %>%
   summarize(gdpPercapweighted = weighted.mean(x = gdpPercap,w = pop),pop = sum(as.numeric(pop)))
 
-plot2 <- ggplot(gapminder_without_Kuwait, aes(x=year, y= gdpPercap, size=pop/100000))+
-  geom_line(aes(group=country, size=0.2)) +
-  geom_point()+
+plot2 <- ggplot(gapminder_without_Kuwait, aes(x=year, y= gdpPercap))+
+  geom_line(aes(color=continent,group=country)) +
+  geom_point(aes(color=continent, group=country))+
   geom_line(data=gapminder_continent,aes(x=year,y=gdpPercapweighted)) + 
-  geom_point(data=gapminder_continent,aes(x=year,y=gdpPercapweighted)) +
-  scale_size_continuous(trans = "sqrt")
+  geom_point(data=gapminder_continent,aes(x=year,y=gdpPercapweighted, size=pop/100000)) +
   facet_wrap(~continent,nrow=1)+
   theme_bw()+
-  labs(x="Wealth and population distribution through time",y="GDP per capita", size="Population (100k)")
-
-gapminder
-gapminder_continent
-
+  labs(x="Wealth and population distribution through time", y="GDP per capita", size="Population (100k)", color="Contintent")
 plot2
-
-ggsave("plot2.png", width=20, units="in")
+ggsave("plot2.png", width=15, units="in")
